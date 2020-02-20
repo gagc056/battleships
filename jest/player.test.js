1f655a8aac61ba629cc3f9b81d97553325cc7028
lhscar <gagc056@gmail.com>
2020-02-20 15:12:47 -0300

    working on the players test

--- jest/player.test.js
+++ jest/player.test.js
@@ -1,9 +1,22 @@
 import playerFactory from '../lib/player';
 import gameboardFactory from '../lib/gameboard';
-describe('When a player is created', () => {
+import shipFactory from '../lib/ship';
+const ship1=shipFactory(4);
+const ship2=shipFactory(4);
+const ship3=shipFactory(4);
+const ship4=shipFactory(4);
+const gameboard1=gameboardFactory();
+const gameboard2=gameboardFactory();
+gameboard1.placeShip(ship1, 0, 0, 'HORIZONTAL');
+gameboard1.placeShip(ship2, 1, 0, 'VERTICAL');
+gameboard2.placeShip(ship3, 4, 4, 'HORIZONTAL');
+gameboard2.placeShip(ship4, 5, 4, 'VERTICAL');
+const player1 = playerFactory('Gaston', 'red');
+const player2 = playerFactory('Amanda', 'blue');
+
+
+describe('playerFactory', () => {
   test(' Player 1  and Player 2 created', () => {
-    const player1 = playerFactory('Gaston', 'red');
-    const player2 = playerFactory('Amanda', 'blue');
     expect(player1).toMatchObject({
       name: 'Gaston',
       color: 'red',
@@ -13,23 +26,17 @@ describe('When a player is created', () => {
       color: 'blue',
     });
   });
+
+  test('makeAttack', ()=>{
+    expect(gameboard2.statusAt(3,3)).toBe(null)
+    player1.makeAttack(gameboard2,3,3);
+    expect(gameboard2.statusAt(3,3)).toBe('MISS');
+    player2.makeAttack(gameboard1,1,4);
+    expect(gameboard1.statusAt(1,4)).toBe('HIT');
+
+  })
 });
 
-describe('Player: makeAttack() (Player makes attack to grid) ', () => {
-  test('Player 1 makes an attack successfully to grid ', () => {
-    const Player1 = Player('Gaston', 'red');
-    const Gameboard2 = Gameboard[0, 8];
-    Player1.makeAttack(Gameboard2, [0, 0]);
-    expect(Gameboard2.grid[0][0]).toBe('X');
-    expect(Player1.attacksMade).toEqual([[0, 0]]);
-  });
 
-  test('Player 2 makes an attack successfully to grid', () => {
-    const Player2 = Player('Amanda', 'blue');
-    const Gameboard1 = Gameboard(0, 8);
-    Player2.makeAttack(Gameboard1, [0, 1]);
-    expect(Gameboard1.grid[0][1]).toBe('X');
-    expect(Player1.attacksMade).toEqual([[0, 1]]);
-  });
 
-});
+
